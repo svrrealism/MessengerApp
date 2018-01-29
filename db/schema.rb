add3b311fa62fa955e83102695bf4e9de202e31d
svrrealism <svrrealists@gmail.com>
2018-01-28 19:55:08 -0500

    Add messages view and styles.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 ActiveRecord::Schema.define(version: 20180128235849) do
 
   create_table "messages", force: :cascade do |t|
-    #create column called content in the messages tables
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
