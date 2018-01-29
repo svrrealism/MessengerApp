37336107fdab0aa944e5c275432b47ef492746a8
svrrealism <svrrealists@gmail.com>
2018-01-28 19:12:04 -0500

    Add comment to schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20180128235849) do
 
   create_table "messages", force: :cascade do |t|
+    #create column called content in the messages tables
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
