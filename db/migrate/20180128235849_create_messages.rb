05505aa23285841ad2599a98b618b9a3da33317e
svrrealism <svrrealists@gmail.com>
2018-01-28 19:13:26 -0500

    Add comment to correct file.

--- db/migrate/20180128235849_create_messages.rb
+++ db/migrate/20180128235849_create_messages.rb
@@ -1,6 +1,7 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
   def change
     create_table :messages do |t|
+      #create column called content in the messages tables
       t.text :content
       t.timestamps
     end
