f49c3364bd0f7cc92e19b4147b0d5f445e396359
svrrealism <svrrealists@gmail.com>
2018-01-28 19:14:50 -0500

    More commentary on the migrate file.

--- db/migrate/20180128235849_create_messages.rb
+++ db/migrate/20180128235849_create_messages.rb
@@ -1,4 +1,5 @@
 class CreateMessages < ActiveRecord::Migration[5.1]
+  #tells rails what changes to make to the database.
   def change
     create_table :messages do |t|
       #create column called content in the messages tables
